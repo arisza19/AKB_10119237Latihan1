d700c72a0b49e476d4e222f09b88073ff5267464
Arisza Fathurrahman <sazaheslin23@gmail.com>
2022-04-19 10:31:32 +0700

    Latihan 1

--- app/src/main/java/com/example/a10119237latihan1/HomeActivity.java
+++ app/src/main/java/com/example/a10119237latihan1/HomeActivity.java
@@ -4,6 +4,14 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+/*
+Developed by AriszaZufarFathurrahman
+NIM : 10119237
+Nama : Arisza Zufar Fathurrahman
+Kelas : IF-6
+on Tuesday, April 19 2022
+*/
+
 public class HomeActivity extends AppCompatActivity {
 
     @Override
